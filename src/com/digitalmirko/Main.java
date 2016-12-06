04f527ea565595c275cc5211146f3bbecde9a256
Mirko Cukich <mirkocukich@gmail.com>
2016-12-06 14:52:52 -0600

    Program Complete

--- src/com/digitalmirko/Main.java
+++ src/com/digitalmirko/Main.java
@@ -1,9 +1,33 @@
 package com.digitalmirko;
 
-// Iterating though an ArrayList
+// Iterating though an ArrayList using Methods: hasNext & next
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        List<String> myList = new ArrayList<String>();
+        myList.add("Objective-C");
+        myList.add("Swift");
+        myList.add("Java");
+        myList.add("Python");
+        myList.add("XML");
+        Iterator<String> itor = myList.iterator();
+        while(itor.hasNext()) {
+            System.out.println(itor.next());
+        }
     }
 }
+
+/*
+Output:
+Objective-C
+Swift
+Java
+Python
+XML
+ */
